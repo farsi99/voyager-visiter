c4b8b0ad3285bc7c8287140fcede6c37ffa722c1
Farouk <farouksoule@gmail.com>
2020-01-20 09:44:26 +0100

    Creation de profile

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectManager as PersistenceObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AccountController extends AbstractController
@@ -21,20 +25,25 @@ class AccountController extends AbstractController
     }
 
     /**
-     * @Route("/logout", name="account_logout")
-     */
-    public function logout()
-    {
-    }
-
-    /**
      * cette méthode traite l'affichage d'un profil abonné
      * @Route("/profile", name="account_profile")
      * @IsGranted("ROLE_USER")
+     * 
+     * @return Response
      */
     public function profile()
     {
         $user = $this->getUser();
         dump($user);
+        return $this->render('account/profile.html.twig', [
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * @Route("/logout", name="account_logout")
+     */
+    public function logout()
+    {
     }
 }
